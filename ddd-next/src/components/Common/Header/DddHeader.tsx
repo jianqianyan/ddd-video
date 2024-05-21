fae4c1e95da23ac4c64579a810c57347dde2af94
jianqianyan <jianqianyan@qq.com>
2024-05-21 22:23:36 +0800

    style: (header) right entry style

--- ddd-next/src/components/Common/Header/DddHeader.tsx
+++ ddd-next/src/components/Common/Header/DddHeader.tsx
@@ -3,24 +3,26 @@ import HeaderItem from "./HeaderItem"
 import { leftEntryList, rightEntryList } from "./enrtyList"
 export default function DddHeader() {
   return (
-    <div className="w-100 h-16 flex px-6 items-center">
+    <div className="w-100 h-16 flex px-6 items-center min-w-[1100px]">
       <div className="flex justify-start flex-1" >
         {leftEntryList.map((item, index) => (<HeaderItem key={index + 'left-entry'} className="mr-4">{item.title}</HeaderItem>))}
       </div>
-      <div className="w-96"></div>
+      <div className="max-w-96"></div>
       <div className="flex flex-1 justify-end">
         {rightEntryList.map((item, index) => {
-          return (<HeaderItem key={index + 'right-entry'}>
-            <Image
-              src={item.icon} // 确保这里的路径正确
-              width={20} // 根据实际需要设置
-              height={20} // 根据实际需要设置
-              alt={item.title} // 为图像提供替代文本
-            />
-            {item.title}
-          </HeaderItem>)
+          return (
+            <HeaderItem key={index + 'right-entry'} className="flex flex-col justify-center items-center ml-4">
+              <Image
+                src={item.icon}
+                width={20}
+                height={20}
+                alt={item.title}
+              />
+              {item.title}
+            </HeaderItem>
+          )
         })}
-
+        <button>投稿</button>
       </div>
     </div>
   )
