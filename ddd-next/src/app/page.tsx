f6f168982751fd62331683119de140be8b7ab3af
jianqianyan <jianqianyan@qq.com>
2024-05-22 21:47:43 +0800

    feat: add font solutions and format code

--- ddd-next/src/app/page.tsx
+++ ddd-next/src/app/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home({ chilren }) {
-  return (
-    <main>
-      111
-    </main>
-  );
+  return <main>111</main>
 }
--- ddd-next/src/components/Common/Header/DddHeader.tsx
+++ ddd-next/src/components/Common/Header/DddHeader.tsx
@@ -1,18 +1,28 @@
-import Image from "next/image"
-import HeaderItem from "./HeaderItem"
-import { leftEntryList, rightEntryList } from "./enrtyList"
-import SubmissionButton from "./SubmissionButton"
+import Image from 'next/image'
+import HeaderItem from './HeaderItem'
+import { leftEntryList, rightEntryList } from './enrtyList'
+import SubmissionButton from './SubmissionButton'
 export default function DddHeader() {
   return (
-    <div className="w-100 h-16 flex px-6 items-center min-w-[1100px]">
-      <div className="flex justify-start flex-1" >
-        {leftEntryList.map((item, index) => (<HeaderItem key={index + 'left-entry'} className="mr-4">{item.title}</HeaderItem>))}
+    <div className='w-100 h-16 flex px-6 items-center min-w-[1100px]'>
+      <div className='flex justify-start flex-1'>
+        {leftEntryList.map((item, index) => (
+          <HeaderItem
+            key={index + 'left-entry'}
+            className='mr-4'
+          >
+            {item.title}
+          </HeaderItem>
+        ))}
       </div>
-      <div className="max-w-96"></div>
-      <div className="flex flex-1 justify-end">
+      <div className='max-w-96'></div>
+      <div className='flex flex-1 justify-end'>
         {rightEntryList.map((item, index) => {
           return (
-            <HeaderItem key={index + 'right-entry'} className="flex flex-col justify-center items-center ml-4">
+            <HeaderItem
+              key={index + 'right-entry'}
+              className='flex flex-col justify-center items-center ml-4'
+            >
               <item.icon />
               {item.title}
             </HeaderItem>
