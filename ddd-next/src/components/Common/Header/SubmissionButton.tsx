981dfa04671321ede33b441ac0c002d6a7607b7b
jianqianyan <jianqianyan@qq.com>
2024-05-23 22:50:22 +0800

    feat:(header) make submission button

--- ddd-next/src/components/Common/Header/SubmissionButton.tsx
+++ ddd-next/src/components/Common/Header/SubmissionButton.tsx
@@ -1,33 +1,76 @@
+'use client'
 import UpdateIcon from './svgs/update.svg'
+import './style.css'
+import React, { useState, useRef, useEffect } from 'react'
+import { submissionList } from './submissionList'
 
 export default function SubmissionButton() {
+  const [isHovered, setIsHovered] = useState(false)
+  const [isPopoverVisible, setIsPopoverVisible] = useState(false)
+
+  const timer = useRef<NodeJS.Timeout | null>(null)
+
+  const handleMouseEnter = () => {
+    if (timer) {
+      clearTimeout(timer.current)
+    }
+    setIsPopoverVisible(true)
+    setTimeout(() => {
+      setIsHovered(true)
+    }, 10)
+  }
+
+  const handleMouseLeave = () => {
+    timer.current = setTimeout(() => {
+      setIsHovered(false)
+      setTimeout(() => {
+        setIsPopoverVisible(false)
+      }, 300)
+    }, 300)
+  }
+
+  useEffect(() => {
+    if (isHovered) {
+      if (timer.current) {
+        clearTimeout(timer.current)
+      }
+      setIsPopoverVisible(true)
+    }
+  }, [isHovered])
+
   return (
-    <a className='ml-2'>
-      <div
-        style={{
-          background: '#fb7299',
-          color: '#fff',
-          height: '34px',
-          width: '90px',
-          alignItems: 'center',
-          justifyContent: 'center',
-          borderRadius: '8px',
-          display: 'flex',
-          cursor: 'pointer',
-          fontSize: '14px',
-          fontFamily:
-            'PingFang SC,HarmonyOS_Medium,Helvetica Neue,Microsoft YaHei,sans-serif',
-          fontWeight: 500,
-        }}
-      >
-        <UpdateIcon
-          style={{
-            fontSize: '18px',
-            marginRight: '5px',
-          }}
-        />
-        <span>投稿</span>
-      </div>
-    </a>
+    <div className='ml-2 relative'>
+      <a>
+        <div
+          className='submission-button'
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <UpdateIcon className='update-icon' />
+          <span>投稿</span>
+        </div>
+      </a>
+      {isPopoverVisible && (
+        <div
+          className={`popover-box ${isHovered ? 'show' : ''}`}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <div className='popover-content'>
+            <div className='popover-content-innner'>
+              {submissionList.map((item, index) => (
+                <a
+                  key={index}
+                  className='upload-item'
+                >
+                  <item.icon className='item-icon'></item.icon>
+                  <div className='item-title'>{item.title}</div>
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
---
+++ ddd-next/src/components/Common/Header/submissionList.ts
@@ -0,0 +1,33 @@
+import columnSubmission from './svgs/columnSubmission.svg'
+import audioSubmission from './svgs/audioSubmission.svg'
+import stickerSubmission from './svgs/stickerSubmission.svg'
+import videoSubmission from './svgs/videoSubmission.svg'
+import submissionManagement from './svgs/submissionManagement.svg'
+
+export const submissionList = [
+  {
+    title: '专栏投稿',
+    icon: columnSubmission,
+    link: '',
+  },
+  {
+    title: '音频投稿',
+    icon: audioSubmission,
+    link: '',
+  },
+  {
+    title: '贴纸投稿',
+    icon: stickerSubmission,
+    link: '',
+  },
+  {
+    title: '视频投稿',
+    icon: videoSubmission,
+    link: '',
+  },
+  {
+    title: '投稿管理',
+    icon: submissionManagement,
+    link: '',
+  },
+]
